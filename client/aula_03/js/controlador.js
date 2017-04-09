fe0316b1925c3f5aadf193f3da7cf46212e8f54e
diego <diego@phonecat.com.br>
2017-04-08 23:23:06 -0300

    filter

--- client/aula_03/js/controlador.js
+++ client/aula_03/js/controlador.js
@@ -1,3 +1,5 @@
+'use strict';
+
 angular.module('start', []) //necessita colocar o ng-app na index.html para puxar o modulo
 
 .controller('indexController', indexController); // necessita colocar o ng-controller para puxar o controller
@@ -5,6 +7,18 @@ angular.module('start', []) //necessita colocar o ng-app na index.html para puxa
 function indexController($scope){
 	// a funcao deve ter o mesmo nome do controller
 	alert("iniciando controller");
+
+	filter();
+
+	function filter(){
+		var array = [1,2,3,4,5];
+
+		array = array.filter(function(i){
+			return i > 3;
+		});
+
+		console.log(array);
+	}
 
 	/* tudo que for utilizado com a var $scope ficará visivel para a index.html
 	Ex: vamos criar uma var escrito "hello world" e mostrar na tela */
