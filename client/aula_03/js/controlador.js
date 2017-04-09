dbb8b4fcdf4f80c7445d1f3441c26ba827e8cfc7
diego <diego@phonecat.com.br>
2017-04-08 22:02:29 -0300

    aula 03 e correcoes

---
+++ client/aula_03/js/controlador.js
@@ -0,0 +1,24 @@
+angular.module('start', []) //necessita colocar o ng-app na index.html para puxar o modulo
+
+.controller('indexController', indexController); // necessita colocar o ng-controller para puxar o controller
+
+function indexController($scope){
+	// a funcao deve ter o mesmo nome do controller
+	alert("iniciando controller");
+
+	/* tudo que for utilizado com a var $scope ficará visivel para a index.html
+	Ex: vamos criar uma var escrito "hello world" e mostrar na tela */
+
+	var hello = "Hello World";
+
+	//agora vamos deixa-la visivel para a index
+
+	$scope.hello = hello;
+
+
+	$scope.mudar = function(){ //na chamada de funcao no angular, a funcao necessita estar nesse padrao, sendo chamada pelo $scope
+		$scope.hello = $scope.titulo; //mudando o titulo do site
+	}
+
+
+}
